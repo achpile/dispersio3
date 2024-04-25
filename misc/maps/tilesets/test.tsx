af6b5a0840934d5b3f36a4e09d7b60112878cac3
Fedosov Alexander <achpile@gmail.com>
2024-04-25 16:57:00 +0300

    Added test map

---
+++ misc/maps/tilesets/test.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="test" tilewidth="16" tileheight="16" tilecount="28" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/beam_wooden.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/beam_wooden_top.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_d1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_d2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_hd11.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_hd12.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_hd21.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_hd22.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_left.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_right.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_d1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_d2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_fd1.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_fd2.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_fhd1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_fhd2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_hd11.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_hd12.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_hd21.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_hd22.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_left.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/brick_top_right.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/cave.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/cave_bot.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/platform.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/test/wall.png"/>
+ </tile>
+</tileset>
