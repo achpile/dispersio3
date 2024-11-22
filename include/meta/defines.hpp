a45ee20ac7d09ace47fa1f0c03803e1fa83d6e4e
Fedosov Alexander <achpile@gmail.com>
2024-11-22 11:14:00 +0300

    Increased version

--- include/meta/defines.hpp
+++ include/meta/defines.hpp
@@ -7,7 +7,7 @@
 
 ***********************************************************************/
 #define PROJECT_NAME          "Dispersio 3"
-#define PROJECT_VERS          "0.3.0"
+#define PROJECT_VERS          "0.3.1"
 
 
 
