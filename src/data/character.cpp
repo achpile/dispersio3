0be0db14778e0ce07d0a9e1bc1dfa373a6b50f07
Fedosov Alexander <achpile@gmail.com>
2024-11-26 18:36:25 +0300

    Character datamodel split

--- src/data/character.cpp
+++ src/data/character.cpp
@@ -8,17 +8,18 @@
 ***********************************************************************/
 ach::DataCharacter::DataCharacter(json_t *obj)
 {
-	body    = db->getBody  (json_object_get_string(obj, "Body"  ));
-	weapon  = db->getWeapon(json_object_get_string(obj, "Weapon"));
-	chunks  = db->getSheet (json_object_get_string(obj, "Chunks"));
+	body    = db->getBody  (json_object_get_branch_string(obj, "Appearance.Body" ));
+	chunks  = db->getSheet (json_object_get_branch_string(obj, "Appearance.Chunk"));
+	weapon  = db->getWeapon(json_object_get_branch_string(obj, "Game.Weapon"     ));
 
-	barrel  = json_object_get_real(obj, "Barrel");
+	barrel  = json_object_get_branch_real(obj, "Appearance.Barrel");
 
-	ai      = (ach::AIType)pair_get_enum(json_object_get_string(obj, "AI"), pairAI);
-	hitbox  = vector_json_coord(json_object_get(obj, "Hitbox"), "X", "Y");
-	color   = str_to_color(json_object_get_string(obj, "Color"));
-	blood   = str_to_color(json_object_get_string(obj, "Blood"));
-	chunk   = str_to_color(json_object_get_string(obj, "Chunk"));
+	ai      = (ach::AIType)pair_get_enum(json_object_get_branch_string(obj, "Game.AI"), pairAI);
+	hitbox  = vector_json_coord(json_object_get_branch(obj, "Game.Hitbox"), "X", "Y");
+
+	color   = str_to_color(json_object_get_branch_string(obj, "Color.Model"));
+	blood   = str_to_color(json_object_get_branch_string(obj, "Color.Blood"));
+	chunk   = str_to_color(json_object_get_branch_string(obj, "Color.Chunk"));
 
 	speed   = json_object_get_branch_real   (obj, "Stats.Speed" );
 	jumping = json_object_get_branch_real   (obj, "Stats.Jump"  );
