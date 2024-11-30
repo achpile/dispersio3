ed0fabcedfc95e062f3427be22365fbcd3e1a002
Fedosov Alexander <achpile@gmail.com>
2024-11-30 21:30:02 +0300

    Development tileset

---
+++ misc/tiled/tilesets/dev.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Dev" tilewidth="32" tileheight="32" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_ld1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_ld2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_lu1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_lu2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_rd1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_rd2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_ru1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c22_ru2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c45_ld.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c45_lu.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c45_rd.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/c45_ru.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/fill.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/platform.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../../../data/base/gfx/game/tiles/dev/wall.png"/>
+ </tile>
+</tileset>
