6714a04f962092eb766321456efd7880ca69c32f
Fedosov Alexander <achpile@gmail.com>
2025-02-16 11:42:41 +0300

    Collectable bag sheet

--- misc/tiled/tilesets/dev.tsx
+++ misc/tiled/tilesets/dev.tsx
@@ -2,51 +2,51 @@
 <tileset version="1.8" tiledversion="1.8.2" name="Dev" tilewidth="16" tileheight="16" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_ld1.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ld1.png"/>
  </tile>
  <tile id="1">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_ld2.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ld2.png"/>
  </tile>
  <tile id="2">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_lu1.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_lu1.png"/>
  </tile>
  <tile id="3">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_lu2.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_lu2.png"/>
  </tile>
  <tile id="4">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_rd1.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_rd1.png"/>
  </tile>
  <tile id="5">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_rd2.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_rd2.png"/>
  </tile>
  <tile id="6">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_ru1.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ru1.png"/>
  </tile>
  <tile id="7">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c22_ru2.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ru2.png"/>
  </tile>
  <tile id="8">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c45_ld.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_ld.png"/>
  </tile>
  <tile id="9">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c45_lu.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_lu.png"/>
  </tile>
  <tile id="10">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c45_rd.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_rd.png"/>
  </tile>
  <tile id="11">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/c45_ru.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_ru.png"/>
  </tile>
  <tile id="12">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/fill.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/fill.png"/>
  </tile>
  <tile id="13">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/platform.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/platform.png"/>
  </tile>
  <tile id="14">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/wall.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/wall.png"/>
  </tile>
  <tile id="15">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tiles/dev/spike.png"/>
+  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/spike.png"/>
  </tile>
 </tileset>
