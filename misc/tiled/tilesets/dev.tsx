871e9bfa4380d1dfb2b4dc3987bb58cff3fbb4e7
Fedosov Alexander <achpile@gmail.com>
2025-02-17 12:37:37 +0300

    Updated tiled files

--- misc/tiled/tilesets/dev.tsx
+++ misc/tiled/tilesets/dev.tsx
@@ -1,52 +1,52 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Dev" tilewidth="16" tileheight="16" tilecount="16" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Dev" tilewidth="16" tileheight="16" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ld1.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_ld1.png" width="16" height="16"/>
  </tile>
  <tile id="1">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ld2.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_ld2.png" width="16" height="16"/>
  </tile>
  <tile id="2">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_lu1.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_lu1.png" width="16" height="16"/>
  </tile>
  <tile id="3">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_lu2.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_lu2.png" width="16" height="16"/>
  </tile>
  <tile id="4">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_rd1.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_rd1.png" width="16" height="16"/>
  </tile>
  <tile id="5">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_rd2.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_rd2.png" width="16" height="16"/>
  </tile>
  <tile id="6">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ru1.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_ru1.png" width="16" height="16"/>
  </tile>
  <tile id="7">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c22_ru2.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c22_ru2.png" width="16" height="16"/>
  </tile>
  <tile id="8">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_ld.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c45_ld.png" width="16" height="16"/>
  </tile>
  <tile id="9">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_lu.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c45_lu.png" width="16" height="16"/>
  </tile>
  <tile id="10">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_rd.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c45_rd.png" width="16" height="16"/>
  </tile>
  <tile id="11">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/c45_ru.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/c45_ru.png" width="16" height="16"/>
  </tile>
  <tile id="12">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/fill.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/fill.png" width="16" height="16"/>
  </tile>
  <tile id="13">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/platform.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/platform.png" width="16" height="16"/>
  </tile>
  <tile id="14">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/wall.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/wall.png" width="16" height="16"/>
  </tile>
  <tile id="15">
-  <image width="16" height="16" source="../../../data/base/gfx/game/tile/dev/spike.png"/>
+  <image source="../../../data/base/gfx/game/tile/dev/spike.png" width="16" height="16"/>
  </tile>
 </tileset>
