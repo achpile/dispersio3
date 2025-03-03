ae7db4ca408d18b26086347085bd564cf473a41b
Fedosov Alexander <achpile@gmail.com>
2025-03-03 18:36:34 +0300

    Black tile and misc tileset

---
+++ misc/tiled/tilesets/misc.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Misc" tilewidth="16" tileheight="16" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/misc/black.png" width="16" height="16"/>
+ </tile>
+</tileset>
