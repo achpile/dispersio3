9751fc59345a270c945595a835059263791443ea
Fedosov Alexander <achpile@gmail.com>
2025-03-13 15:02:39 +0300

    Rock and Dirt tiles

---
+++ misc/tiled/tilesets/nature.blocks.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Nature Blocks" tilewidth="16" tileheight="16" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/nature/dirt_top.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/nature/dirt_mid.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/nature/dirt_bot.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/nature/rock_top.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/nature/rock_mid.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/nature/rock_bot.png" width="16" height="16"/>
+ </tile>
+</tileset>
