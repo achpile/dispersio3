fd45d0475b298851a106089e7b0dedbbc62ae3b3
Fedosov Alexander <achpile@gmail.com>
2025-03-13 18:07:24 +0300

    Folders renaming

---
+++ misc/tiled/tilesets/block.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Block" tilewidth="16" tileheight="16" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/block/concrete.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_h_left.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_h_mid.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_h_right.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_v_top.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_v_mid.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_v_bot.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_tl.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_tm.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_tr.png" width="16" height="16"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_ml.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_m.png" width="16" height="16"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_mr.png" width="16" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_bl.png" width="16" height="16"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_bm.png" width="16" height="16"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_b_br.png" width="16" height="16"/>
+ </tile>
+</tileset>
