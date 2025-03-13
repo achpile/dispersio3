8fcda0deb8290c5434eada7905de9900942e9477
Fedosov Alexander <achpile@gmail.com>
2025-03-13 16:27:05 +0300

    Concrete block sprite

--- misc/tiled/tilesets/bridges.tsx
+++ misc/tiled/tilesets/bridges.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Bridges" tilewidth="16" tileheight="16" tilecount="10" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Bridges" tilewidth="16" tileheight="16" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../../data/base/gfx/game/tile/bridges/metal_slope_l_1.png" width="16" height="16"/>
@@ -30,5 +30,20 @@
  </tile>
  <tile id="9">
   <image source="../../../data/base/gfx/game/tile/bridges/metal_ladder_r_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_bridge_left.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_bridge_mid.png" width="16" height="16"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_bridge_right.png" width="16" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_bridge_support_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_bridge_support_rt.png" width="16" height="16"/>
  </tile>
 </tileset>
