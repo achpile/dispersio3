29640ccc9ee0f59f7a1b8d98de3321f2f4e0eca5
Fedosov Alexander <achpile@gmail.com>
2025-03-13 15:18:43 +0300

    Bridges tileset

---
+++ misc/tiled/tilesets/bridges.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Bridges" tilewidth="16" tileheight="16" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_slope_l_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_slope_l_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_slope_l_3.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_slope_r_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_slope_r_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/bridges/metal_slope_r_3.png" width="16" height="16"/>
+ </tile>
+</tileset>
