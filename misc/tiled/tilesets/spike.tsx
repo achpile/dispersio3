a8d88f54845981434a3074bd98661831af6c493f
Fedosov Alexander <achpile@gmail.com>
2025-03-14 15:28:48 +0300

    Spikes tileset

---
+++ misc/tiled/tilesets/spike.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Spike" tilewidth="16" tileheight="16" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/spike/short_d.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/spike/short_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/spike/short_r.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/spike/short_u.png" width="16" height="16"/>
+ </tile>
+</tileset>
