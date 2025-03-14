2ea81c1b36054c91836ae54a1b7af8066310acbf
Fedosov Alexander <achpile@gmail.com>
2025-03-14 12:32:40 +0300

    Wall tileset

---
+++ misc/tiled/tilesets/base.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Base" tilewidth="16" tileheight="16" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/base/wall_m.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/base/wall_t.png" width="16" height="16"/>
+ </tile>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/base/wall_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/base/wall_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/base/wall_r.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/base/wall_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/base/wall_lb.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/base/wall_rb.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/base/wall_rt.png" width="16" height="16"/>
+ </tile>
+</tileset>
