63b8ff927e197cd8332e3e79142e1a955de62ff6
Fedosov Alexander <achpile@gmail.com>
2025-03-14 11:02:21 +0300

    Beams and ropes

---
+++ misc/tiled/tilesets/beam.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Beam" tilewidth="16" tileheight="16" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_h_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_h_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_v_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_v_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_v_1_half.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_v_2_half.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_rope.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_rope_end.png" width="16" height="16"/>
+ </tile>
+</tileset>
