89adddfbe52c0cce9b918fc98a49730b4faa5047
Fedosov Alexander <achpile@gmail.com>
2025-03-14 09:56:12 +0300

    Bridges refactoring & Moving metal platform

--- misc/tiled/tilesets/bridge.tsx
+++ misc/tiled/tilesets/bridge.tsx
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Bridge" tilewidth="16" tileheight="16" tilecount="15" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Bridge" tilewidth="16" tileheight="16" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_slope_l_1.png" width="16" height="16"/>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_slope_l_3.png" width="16" height="16"/>
  </tile>
  <tile id="1">
   <image source="../../../data/base/gfx/game/tile/bridge/metal_slope_l_2.png" width="16" height="16"/>
  </tile>
- <tile id="2">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_slope_l_3.png" width="16" height="16"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_slope_l_1.png" width="16" height="16"/>
  </tile>
  <tile id="3">
   <image source="../../../data/base/gfx/game/tile/bridge/metal_slope_r_1.png" width="16" height="16"/>
@@ -20,30 +20,27 @@
   <image source="../../../data/base/gfx/game/tile/bridge/metal_slope_r_3.png" width="16" height="16"/>
  </tile>
  <tile id="6">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_ladder_l_1.png" width="16" height="16"/>
- </tile>
- <tile id="7">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_ladder_l_2.png" width="16" height="16"/>
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_ladder_l.png" width="16" height="16"/>
  </tile>
  <tile id="8">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_ladder_r_1.png" width="16" height="16"/>
- </tile>
- <tile id="9">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_ladder_r_2.png" width="16" height="16"/>
- </tile>
- <tile id="10">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_bridge_left.png" width="16" height="16"/>
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_ladder_r.png" width="16" height="16"/>
  </tile>
  <tile id="11">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_bridge_mid.png" width="16" height="16"/>
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_bridge.png" width="16" height="16"/>
  </tile>
- <tile id="12">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_bridge_right.png" width="16" height="16"/>
+ <tile id="15">
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_bridge_2.png" width="16" height="16"/>
  </tile>
- <tile id="13">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_bridge_support_lt.png" width="16" height="16"/>
+ <tile id="22">
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_support_lb.png" width="16" height="16"/>
  </tile>
- <tile id="14">
-  <image source="../../../data/base/gfx/game/tile/bridge/metal_bridge_support_rt.png" width="16" height="16"/>
+ <tile id="23">
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_support_rb.png" width="16" height="16"/>
+ </tile>
+ <tile id="24">
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_support_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="25">
+  <image source="../../../data/base/gfx/game/tile/bridge/metal_support_rt.png" width="16" height="16"/>
  </tile>
 </tileset>
