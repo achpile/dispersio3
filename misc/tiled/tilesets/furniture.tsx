3f117439045693e9fa5942185c35b65fe58e0f37
Fedosov Alexander <achpile@gmail.com>
2025-03-15 15:42:19 +0300

    Furniture

---
+++ misc/tiled/tilesets/furniture.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Furniture" tilewidth="16" tileheight="16" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/furniture/desktop.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/furniture/terminal.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/furniture/radio.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/furniture/table.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/furniture/cam_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/furniture/cam_lb.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/furniture/cam_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/furniture/cam_rb.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/furniture/cam_r.png" width="16" height="16"/>
+ </tile>
+</tileset>
