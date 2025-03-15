6fa143acb1aaf955e99b0971fd2651d6f31a5684
Fedosov Alexander <achpile@gmail.com>
2025-03-15 13:25:14 +0300

    Brick tileset

---
+++ misc/tiled/tilesets/brick.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Brick" tilewidth="16" tileheight="16" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/brick/grey.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s45_lb.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s45_rb.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s45_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s45_rt.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s22_lb_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s22_lb_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s22_rb_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/brick/grey_s22_rb_1.png" width="16" height="16"/>
+ </tile>
+</tileset>
