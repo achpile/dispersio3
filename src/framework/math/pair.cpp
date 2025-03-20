32d58ee7aaca86fa7c9b0944c4ec8b8f9070eb44
Fedosov Alexander <achpile@gmail.com>
2025-03-20 11:28:43 +0300

    None direction enum

--- src/framework/math/pair.cpp
+++ src/framework/math/pair.cpp
@@ -11,6 +11,7 @@ ach::Pair<ach::Direction> pairDirection[] =
 	{.num = ach::Direction::dDown , .str = "down" },
 	{.num = ach::Direction::dLeft , .str = "left" },
 	{.num = ach::Direction::dRight, .str = "right"},
+	{.num = ach::Direction::dCount, .str = "none" },
 
 	{.num = ach::Direction::dCount, .str = NULL   },
 };
@@ -25,6 +26,7 @@ ach::Pair<ach::Orientation> pairOrientation[] =
 {
 	{.num = ach::Orientation::oVertical  , .str = "vertical"  },
 	{.num = ach::Orientation::oHorizontal, .str = "horizontal"},
+	{.num = ach::Orientation::oCount     , .str = "none"      },
 
 	{.num = ach::Orientation::oCount     , .str = NULL        },
 };
