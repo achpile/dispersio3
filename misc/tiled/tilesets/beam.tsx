6b68096396b7113223d72c2e7fa006a8718a0c72
Fedosov Alexander <achpile@gmail.com>
2025-03-21 10:46:17 +0300

    LaunchPad map: decoration

--- misc/tiled/tilesets/beam.tsx
+++ misc/tiled/tilesets/beam.tsx
@@ -1,6 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Beam" tilewidth="16" tileheight="16" tilecount="8" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Beam" tilewidth="16" tileheight="16" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="10">
+  <image source="../../../data/base/gfx/game/tile/beam/metal_c.png" width="16" height="16"/>
+ </tile>
  <tile id="2">
   <image source="../../../data/base/gfx/game/tile/beam/metal_h_1.png" width="16" height="16"/>
  </tile>
