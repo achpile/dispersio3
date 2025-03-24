8f227e6883c715eff6b76d8dfcceb2859f4229bd
Fedosov Alexander <achpile@gmail.com>
2025-03-24 19:38:57 +0300

    Rocket map: WIP

--- misc/tiled/tilesets/block.tsx
+++ misc/tiled/tilesets/block.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Block" tilewidth="16" tileheight="16" tilecount="20" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Block" tilewidth="16" tileheight="16" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../../data/base/gfx/game/tile/block/concrete.png" width="16" height="16"/>
@@ -51,6 +51,15 @@
  </tile>
  <tile id="16">
   <image source="../../../data/base/gfx/game/tile/block/concrete_corner_tr.png" width="16" height="16"/>
+ </tile>
+ <tile id="26">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_corner_tl.png" width="16" height="16"/>
+ </tile>
+ <tile id="24">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_corner_bl.png" width="16" height="16"/>
+ </tile>
+ <tile id="25">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_corner_br.png" width="16" height="16"/>
  </tile>
  <tile id="18">
   <image source="../../../data/base/gfx/game/tile/block/decor_top.png" width="16" height="16"/>
