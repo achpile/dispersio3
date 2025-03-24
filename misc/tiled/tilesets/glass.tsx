d98ea588c67218bc8099af7fcf25ee10bc2cae2c
Fedosov Alexander <achpile@gmail.com>
2025-03-24 17:39:03 +0300

    Glass tileset

---
+++ misc/tiled/tilesets/glass.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Glass" tilewidth="16" tileheight="16" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/glass/glass.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_r.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_t.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_rt.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_lb.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_rb.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../data/base/gfx/game/tile/glass/glass_corner_rt.png" width="16" height="16"/>
+ </tile>
+</tileset>
