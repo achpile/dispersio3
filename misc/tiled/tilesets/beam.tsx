d1947f7e4a8fcaedf6c6b8b5f54f65b9e09a3481
Fedosov Alexander <achpile@gmail.com>
2025-03-24 20:37:27 +0300

    Beam tileset rework

--- misc/tiled/tilesets/beam.tsx
+++ misc/tiled/tilesets/beam.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Beam" tilewidth="16" tileheight="16" tilecount="9" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Beam" tilewidth="16" tileheight="16" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="10">
   <image source="../../../data/base/gfx/game/tile/beam/metal_c.png" width="16" height="16"/>
@@ -15,12 +15,6 @@
  </tile>
  <tile id="5">
   <image source="../../../data/base/gfx/game/tile/beam/metal_v_2.png" width="16" height="16"/>
- </tile>
- <tile id="6">
-  <image source="../../../data/base/gfx/game/tile/beam/metal_v_1_half.png" width="16" height="16"/>
- </tile>
- <tile id="7">
-  <image source="../../../data/base/gfx/game/tile/beam/metal_v_2_half.png" width="16" height="16"/>
  </tile>
  <tile id="8">
   <image source="../../../data/base/gfx/game/tile/beam/metal_rope.png" width="16" height="16"/>
