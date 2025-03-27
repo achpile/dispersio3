0a6774596c3570d0042a2639daa44ff2661c9dd6
Fedosov Alexander <achpile@gmail.com>
2025-03-27 07:38:29 +0300

    Rocket wings decor

--- src/map/object/decor.cpp
+++ src/map/object/decor.cpp
@@ -9,6 +9,12 @@
 ach::MapObjectDecor::MapObjectDecor(ach::ProcessWorld *_world, json_t *obj) : MapObject(_world, obj)
 {
 	setSheet(json_object_get_string(obj, "name"));
+
+	if (json_property_get_boolean(obj, "FlipX"))
+		model->scale.x = -1.0f;
+
+	if (json_property_get_boolean(obj, "FlipY"))
+		model->scale.y = -1.0f;
 }
 
 
