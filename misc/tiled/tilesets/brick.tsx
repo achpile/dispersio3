a89146c6aea08f3fc08d6fb4d093024c9b8a46f0
Fedosov Alexander <achpile@gmail.com>
2025-04-21 08:37:47 +0300

    Red brick tiles

--- misc/tiled/tilesets/brick.tsx
+++ misc/tiled/tilesets/brick.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Brick" tilewidth="16" tileheight="16" tilecount="10" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Brick" tilewidth="16" tileheight="16" tilecount="21" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../../data/base/gfx/game/tile/brick/grey.png" width="16" height="16"/>
@@ -30,5 +30,38 @@
  </tile>
  <tile id="8">
   <image source="../../../data/base/gfx/game/tile/brick/grey_s22_rb_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../data/base/gfx/game/tile/brick/red.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../data/base/gfx/game/tile/brick/red_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s45_lb.png" width="16" height="16"/>
+ </tile>
+ <tile id="18">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s45_rb.png" width="16" height="16"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s45_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s45_rt.png" width="16" height="16"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s22_lb_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s22_lb_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s22_rb_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../data/base/gfx/game/tile/brick/red_s22_rb_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../data/base/gfx/game/tile/brick/back.png" width="16" height="16"/>
  </tile>
 </tileset>
