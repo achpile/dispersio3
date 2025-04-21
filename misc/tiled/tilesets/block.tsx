9b44763c8422dcd2adec342bdc3ddae211430fad
Fedosov Alexander <achpile@gmail.com>
2025-04-21 09:11:16 +0300

    Factory map: WIP

--- misc/tiled/tilesets/block.tsx
+++ misc/tiled/tilesets/block.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Block" tilewidth="16" tileheight="16" tilecount="23" columns="0">
+<tileset version="1.11" tiledversion="1.11.2" name="Block" tilewidth="16" tileheight="16" tilecount="26" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../../data/base/gfx/game/tile/block/concrete.png" width="16" height="16"/>
@@ -69,5 +69,14 @@
  </tile>
  <tile id="23">
   <image source="../../../data/base/gfx/game/tile/block/decor_bot_cut.png" width="16" height="16"/>
+ </tile>
+ <tile id="27">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_v_bot_back.png" width="16" height="16"/>
+ </tile>
+ <tile id="28">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_v_mid_back.png" width="16" height="16"/>
+ </tile>
+ <tile id="29">
+  <image source="../../../data/base/gfx/game/tile/block/concrete_v_top_back.png" width="16" height="16"/>
  </tile>
 </tileset>
