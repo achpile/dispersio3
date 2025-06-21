e4688d97904421ab8b53c30ea94a4297745807d3
Fedosov Alexander <achpile@gmail.com>
2025-06-21 11:22:37 +0300

    Slab tileset

---
+++ misc/tiled/tilesets/slab.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="Slab" tilewidth="16" tileheight="16" tilecount="22" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/slab/block.png" width="16" height="16"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../data/base/gfx/game/tile/slab/lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="18">
+  <image source="../../../data/base/gfx/game/tile/slab/t.png" width="16" height="16"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../data/base/gfx/game/tile/slab/rt.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../data/base/gfx/game/tile/slab/l.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/slab/c.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../data/base/gfx/game/tile/slab/r.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/slab/bl.png" width="16" height="16"/>
+ </tile>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/slab/b.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/slab/br.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/slab/h_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/slab/h_c.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/slab/h_r.png" width="16" height="16"/>
+ </tile>
+ <tile id="21">
+  <image source="../../../data/base/gfx/game/tile/slab/v_t.png" width="16" height="16"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../data/base/gfx/game/tile/slab/v_c.png" width="16" height="16"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../data/base/gfx/game/tile/slab/v_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../data/base/gfx/game/tile/slab/s_l1.png" width="16" height="16"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../data/base/gfx/game/tile/slab/s_l2.png" width="16" height="16"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../data/base/gfx/game/tile/slab/s_t.png" width="16" height="16"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../data/base/gfx/game/tile/slab/s_r2.png" width="16" height="16"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../data/base/gfx/game/tile/slab/s_r1.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/slab/back.png" width="16" height="16"/>
+ </tile>
+</tileset>
