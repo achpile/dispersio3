f6a4a74474d9cbfab8bc66222e7bcaa3be3b701a
Fedosov Alexander <achpile@gmail.com>
2025-08-06 09:16:24 +0300

    Big bushes tiles

--- misc/tiled/tilesets/plant.tsx
+++ misc/tiled/tilesets/plant.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.0" name="Plant" tilewidth="16" tileheight="16" tilecount="22" columns="0">
+<tileset version="1.11" tiledversion="1.11.0" name="Plant" tilewidth="16" tileheight="16" tilecount="37" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../../data/base/gfx/game/tile/plant/grass.png" width="16" height="16"/>
@@ -66,5 +66,50 @@
  </tile>
  <tile id="23">
   <image source="../../../data/base/gfx/game/tile/plant/liana3_bot.png" width="16" height="16"/>
+ </tile>
+ <tile id="28">
+  <image source="../../../data/base/gfx/game/tile/plant/bush1_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="31">
+  <image source="../../../data/base/gfx/game/tile/plant/bush1_t.png" width="16" height="16"/>
+ </tile>
+ <tile id="30">
+  <image source="../../../data/base/gfx/game/tile/plant/bush1_rt.png" width="16" height="16"/>
+ </tile>
+ <tile id="27">
+  <image source="../../../data/base/gfx/game/tile/plant/bush1_lb.png" width="16" height="16"/>
+ </tile>
+ <tile id="26">
+  <image source="../../../data/base/gfx/game/tile/plant/bush1_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="29">
+  <image source="../../../data/base/gfx/game/tile/plant/bush1_rb.png" width="16" height="16"/>
+ </tile>
+ <tile id="34">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_lt.png" width="16" height="16"/>
+ </tile>
+ <tile id="35">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_lt2.png" width="16" height="16"/>
+ </tile>
+ <tile id="39">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_rt2.png" width="16" height="16"/>
+ </tile>
+ <tile id="38">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_rt.png" width="16" height="16"/>
+ </tile>
+ <tile id="32">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_lb.png" width="16" height="16"/>
+ </tile>
+ <tile id="33">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_lb2.png" width="16" height="16"/>
+ </tile>
+ <tile id="40">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="37">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_rb2.png" width="16" height="16"/>
+ </tile>
+ <tile id="36">
+  <image source="../../../data/base/gfx/game/tile/plant/bush2_rb.png" width="16" height="16"/>
  </tile>
 </tileset>
