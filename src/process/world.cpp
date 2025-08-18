08111a2f184c04d17c4e265b3bc990cb31b70feb
Fedosov Alexander <achpile@gmail.com>
2025-08-18 20:04:43 +0300

    Mario map: phys

--- src/process/world.cpp
+++ src/process/world.cpp
@@ -9,7 +9,7 @@
 ach::ProcessWorld::ProcessWorld(ach::StateGame *_owner) : Process(_owner)
 {
 	state  = ach::WorldState::wsFadeIn;
-	map    = new ach::Map(this, db->getMap("Default"));
+	map    = new ach::Map(this, db->getMap("DD-Mario"));
 	player = new ach::Character(this, map->base->player, map->spawn);
 
 	map->cam->follow(&player->phys);
