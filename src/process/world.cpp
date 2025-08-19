302ee8166052b3acc4878d42e0db164840011088
Fedosov Alexander <achpile@gmail.com>
2025-08-19 11:15:41 +0300

    Some renamings

--- src/process/world.cpp
+++ src/process/world.cpp
@@ -9,7 +9,7 @@
 ach::ProcessWorld::ProcessWorld(ach::StateGame *_owner) : Process(_owner)
 {
 	state  = ach::WorldState::wsFadeIn;
-	map    = new ach::Map(this, db->getMap("DD-Mario"));
+	map    = new ach::Map(this, db->getMap("DD-Plumber"));
 	player = new ach::Character(this, map->base->player, map->spawn);
 
 	map->cam->follow(&player->phys);
