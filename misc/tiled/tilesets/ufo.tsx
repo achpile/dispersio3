d162436e5bc3d10d9deb7119c72e4e73822fd1f3
Fedosov Alexander <achpile@gmail.com>
2025-08-23 10:30:47 +0300

    Duck map: UFO tiles

---
+++ misc/tiled/tilesets/ufo.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="Ufo" tilewidth="16" tileheight="16" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="14">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_top_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_top_c.png" width="16" height="16"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_top_r.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_c.png" width="16" height="16"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_r.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_bot_l.png" width="16" height="16"/>
+ </tile>
+ <tile id="0">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_bot_c.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_bot_r.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_d_l_1_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_d_l_2_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_d_l_2_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_d_r_2_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_d_r_2_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_d_r_1_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../data/base/gfx/game/tile/ufo/leg_d_c.png" width="16" height="16"/>
+ </tile>
+</tileset>
