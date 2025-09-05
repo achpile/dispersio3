6d9e908db07f89f2a24763b7c9d2849ef1c8fea9
Fedosov Alexander <achpile@gmail.com>
2025-09-05 10:12:47 +0300

    Added NPC data

--- src/data/npc.cpp
+++ src/data/npc.cpp
@@ -8,7 +8,7 @@
 ***********************************************************************/
 ach::DataNPC::DataNPC(json_t *obj)
 {
-	model  = db->getModel (json_object_get_branch_string(obj, "Appearance.Model"));
+	model  = db->getModel(json_object_get_string(obj, "Model"));
 	wander = json_object_get_boolean(obj, "Wander");
 	speech = json_object_get(obj, "Speech");
 }
