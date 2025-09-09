6f01214d401f4b2f6ca3bbe7aaae3f14d7c3f66e
Fedosov Alexander <achpile@gmail.com>
2025-09-09 12:08:12 +0300

    Race arcade started

--- include/arcade/race.hpp
+++ include/arcade/race.hpp
@@ -6,6 +6,15 @@ namespace ach
 {
 	struct ArcadeRace : Arcade
 	{
+		ach::Timer          ticker;
+
+		sf::RectangleShape *square;
+
+		int pos;
+		int step;
+		int lines[3];
+
+
 		 ArcadeRace(bool select);
 		~ArcadeRace();
 
@@ -13,6 +22,12 @@ namespace ach
 		void process();
 		void draw();
 		void handle();
+
+		void tick();
+
+		void car (int line, int x , sf::Color color);
+		void line(int line        , sf::Color color);
+		void tile(sf::Vector2i pos, sf::Color color);
 	};
 }
 
--- src/arcade/race.cpp
+++ src/arcade/race.cpp
@@ -8,6 +8,9 @@
 ***********************************************************************/
 ach::ArcadeRace::ArcadeRace(bool select) : Arcade(ach::ArcadeGame::Race, select)
 {
+	square = new sf::RectangleShape(sf::Vector2f(ARCADE_SQUARE - 1, ARCADE_SQUARE - 1));
+
+	ticker.set(0.03f);
 }
 
 
@@ -19,6 +22,7 @@ ach::ArcadeRace::ArcadeRace(bool select) : Arcade(ach::ArcadeGame::Race, select)
 ***********************************************************************/
 ach::ArcadeRace::~ArcadeRace()
 {
+	delete square;
 }
 
 
@@ -30,6 +34,8 @@ ach::ArcadeRace::~ArcadeRace()
 ***********************************************************************/
 void ach::ArcadeRace::prepare()
 {
+	pos  = 1;
+	step = 0;
 }
 
 
@@ -41,6 +47,11 @@ void ach::ArcadeRace::prepare()
 ***********************************************************************/
 void ach::ArcadeRace::process()
 {
+	if (!ticker.update(true))
+	{
+		tick();
+		ticker.reset();
+	}
 }
 
 
@@ -52,6 +63,12 @@ void ach::ArcadeRace::process()
 ***********************************************************************/
 void ach::ArcadeRace::draw()
 {
+	line(0, sf::Color::Red);
+	line(1, sf::Color(100, 100, 100));
+	line(2, sf::Color(100, 100, 100));
+	line(3, sf::Color::Red);
+
+	car(pos, 1, sf::Color::Green);
 }
 
 
@@ -63,4 +80,76 @@ void ach::ArcadeRace::draw()
 ***********************************************************************/
 void ach::ArcadeRace::handle()
 {
+	if (ctrl->keys[ach::ControlAction::caUp  ].pressed) pos--;
+	if (ctrl->keys[ach::ControlAction::caDown].pressed) pos++;
+
+	pos = interval_set(pos, 0, 2);
+}
+
+
+
+/***********************************************************************
+     * ArcadeRace
+     * tick
+
+***********************************************************************/
+void ach::ArcadeRace::tick()
+{
+	step--;
+
+	if (step < 0)
+		step = ARCADE_RACE_STEP - 1;
+}
+
+
+
+/***********************************************************************
+     * ArcadeRace
+     * car
+
+***********************************************************************/
+void ach::ArcadeRace::car(int line, int x, sf::Color color)
+{
+	sf::Vector2i p = sf::Vector2i(x, ARCADE_RACE_OFFSET + line * 6);
+
+	tile(p + sf::Vector2i(0, 2), color);
+	tile(p + sf::Vector2i(0, 4), color);
+	tile(p + sf::Vector2i(1, 3), color);
+	tile(p + sf::Vector2i(2, 2), color);
+	tile(p + sf::Vector2i(2, 3), color);
+	tile(p + sf::Vector2i(2, 4), color);
+	tile(p + sf::Vector2i(3, 3), color);
+}
+
+
+
+/***********************************************************************
+     * ArcadeRace
+     * line
+
+***********************************************************************/
+void ach::ArcadeRace::line(int line, sf::Color color)
+{
+	int p = step;
+
+	while (p < ARCADE_RACE_LENGTH)
+	{
+		tile(sf::Vector2i(p, ARCADE_RACE_OFFSET + line * 6), color);
+		p += ARCADE_RACE_STEP;
+	}
+}
+
+
+
+/***********************************************************************
+     * ArcadeRace
+     * tile
+
+***********************************************************************/
+void ach::ArcadeRace::tile(sf::Vector2i pos, sf::Color color)
+{
+	square->setPosition(sf::Vector2f(pos.x * ARCADE_SQUARE - ARCADE_SQUARE / 2.0f, pos.y * ARCADE_SQUARE) + offset);
+	square->setFillColor(color);
+
+	tex->draw(*square);
 }
