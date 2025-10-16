5fd5174a2a5cbed8f43fc9d7bf51c7bb34accad7
Fedosov Alexander <achpile@gmail.com>
2025-10-16 07:59:30 +0300

    Records routines

--- include/container/records.hpp
+++ include/container/records.hpp
@@ -8,11 +8,22 @@ namespace ach
 	{
 		json_t *data;
 
+		json_t *achievements;
+		json_t *highscores;
+		json_t *leaderboards;
 
 		 Records();
 		~Records();
 
 		void save();
+
+		void setAchievement(const char *name);
+		void setHighscore  (const char *name, int score);
+		void setLeaderboard(const char *name, float time);
+
+		bool  getAchievement(const char *name);
+		int   getHighscore  (const char *name);
+		float getLeaderboard(const char *name);
 	};
 }
 
--- src/container/records.cpp
+++ src/container/records.cpp
@@ -9,6 +9,10 @@
 ach::Records::Records()
 {
 	data = json_object_get(dm->data, "Settings");
+
+	achievements = json_object_get(data, "Achievement");
+	highscores   = json_object_get(data, "Highscore"  );
+	leaderboards = json_object_get(data, "Time"       );
 }
 
 
@@ -33,4 +37,94 @@ void ach::Records::save()
 {
 	json_dump_file(data, FILE_RECORDS, JSON_INDENT(4) | JSON_SORT_KEYS);
 	checksum->store(FILE_RECORDS);
+}
+
+
+
+/***********************************************************************
+     * Records
+     * setAchievement
+
+***********************************************************************/
+void ach::Records::setAchievement(const char *name)
+{
+	bool current = getAchievement(name);
+
+	if (current)
+		return;
+
+	json_object_set_boolean(achievements, name, true);
+	save();
+}
+
+
+
+/***********************************************************************
+     * Records
+     * setHighscore
+
+***********************************************************************/
+void ach::Records::setHighscore(const char *name, int score)
+{
+	int current = getHighscore(name);
+
+	if (current >= score)
+		return;
+
+	json_object_set_integer(highscores, name, score);
+	save();
+}
+
+
+
+/***********************************************************************
+     * Records
+     * setLeaderboard
+
+***********************************************************************/
+void ach::Records::setLeaderboard(const char *name, float time)
+{
+	float current = getLeaderboard(name);
+
+	if (current && current <= time)
+		return;
+
+	json_object_set_real(leaderboards, name, time);
+	save();
+}
+
+
+
+/***********************************************************************
+     * Records
+     * getAchievement
+
+***********************************************************************/
+bool ach::Records::getAchievement(const char *name)
+{
+	return json_object_get_boolean(achievements, name);
+}
+
+
+
+/***********************************************************************
+     * Records
+     * getHighscore
+
+***********************************************************************/
+int ach::Records::getHighscore(const char *name)
+{
+	return json_object_get_integer(highscores, name);
+}
+
+
+
+/***********************************************************************
+     * Records
+     * getLeaderboard
+
+***********************************************************************/
+float ach::Records::getLeaderboard(const char *name)
+{
+	return json_object_get_real(leaderboards, name);
 }
