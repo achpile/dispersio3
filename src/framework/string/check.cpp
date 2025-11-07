23200e8c473650eaac85c942c0fa634642364156
Fedosov Alexander <achpile@gmail.com>
2025-11-07 15:14:42 +0300

    Check string functions

--- src/framework/string/check.cpp
+++ src/framework/string/check.cpp
@@ -18,8 +18,17 @@ bool str_check_start(const char *str, const char *start)
 ***********************************************************************/
 bool str_check_name(const char *str)
 {
-// ^[[:alnum:]_-]{1,128}$
-	return str != NULL;
+	if (!str)
+		return false;
+
+	if (!strlen(str) || strlen(str) >= STR_LEN_NAME)
+		return false;
+
+	for (size_t i = 0; i < strlen(str); i++)
+		if (!isalnum(str[i]))
+			return false;
+
+	return true;
 }
 
 
@@ -30,6 +39,18 @@ bool str_check_name(const char *str)
 ***********************************************************************/
 bool str_check_color(const char *str)
 {
-// ^#[[:xdigit:]]{2}{3,4}$
-	return str != NULL;
+	if (!str)
+		return false;
+
+	if (!strlen(str) || (strlen(str) != 7 && strlen(str) != 9))
+		return false;
+
+	if (str[0] != '#')
+		return false;
+
+	for (size_t i = 1; i < strlen(str); i++)
+		if (!isxdigit(str[i]))
+			return false;
+
+	return true;
 }
